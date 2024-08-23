f96f00a74bad03a24f86c4ac71d12ee7f0b327b1
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-23 22:06:20 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -39,20 +39,21 @@ import os
 app = Flask(__name__)
 
 
-DATA_FILE = 'user_data.json'
+# DATA_FILE = 'user_data.json'
 
-# Function to load data from the JSON file
-def load_data():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, 'r') as file:
-            return json.load(file)
-    return {}
+# # Function to load data from the JSON file
+# def load_data():
+#     if os.path.exists(DATA_FILE):
+#         with open(DATA_FILE, 'r') as file:
+#             return json.load(file)
+#     return {}
 
-# Function to save data to the JSON file
-def save_data(data):
-    with open(DATA_FILE, 'w') as file:
-        json.dump(data, file, indent=4)
+# # Function to save data to the JSON file
+# def save_data(data):
+#     with open(DATA_FILE, 'w') as file:
+#         json.dump(data, file, indent=4)
 
+users = ["weasley@1223414432", "dumbledore@1761837661", "lovegood@1846657233", "granger@280282620", "riddle@-931195728", "malfoy@1081489288"]
 
 @app.route("/")
 def hello_world():
@@ -68,24 +69,25 @@ def check():
     pwd = request.form['pwd']
     
     # Load existing data
-    data = load_data()
+    # data = load_data()
     
-    # Store the new input by user ID
-    if user_id in data:
-        data[user_id].append(pwd)
-    else:
-        data[user_id] = [pwd]
+    # # Store the new input by user ID
+    # if user_id in data:
+    #     data[user_id].append(pwd)
+    # else:
+    #     data[user_id] = [pwd]
     
-    # Save data to file
-    save_data(data)
+    # # Save data to file
+    # save_data(data)
     
     # return jsonify({"message": "Data saved successfully!", "data": data})
 
     if request.method == "POST":
-        pwd = request.form['pwd']
-        if pwd == "aurjaoobt":
-            c1 = True
-            ans1 = "first link"
+        if user_id in users:
+        # pwd = request.form['pwd']
+            if pwd == "aurjaoobt":
+                c2 = True
+                ans2 = "first link"
         else:
             return render_template("page.html")
     return render_template("page.html", c1=c2, ans2=ans2)
