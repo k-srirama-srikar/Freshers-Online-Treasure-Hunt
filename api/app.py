e78a1fb90db5389075d6579081f47108fc8b0919
Vishesh <112301037@smail.iitpkd.ac.in>
2024-08-25 02:00:28 +0530

    regarding error image

--- api/app.py
+++ api/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, jsonify, s
 from random import randint
 app = Flask(__name__)
 gen=0
-lst=["1.mp4","2.mp4","3.mp4","4.mp4","5.mp4","6.mp4"]
 def wrongimage():
     global gen
     gen=randint(1,6)
@@ -15,7 +14,7 @@ def hello_world():
 
 @app.route("/check", methods=["GET", "POST"])
 def check():
-    c = False
+    c = "False"
     
 
     pwd = request.form['pwd']
@@ -25,7 +24,7 @@ def check():
         if pwd == "https://www.youtube.com/watch?v=jtaqHzUhYdw":
             return render_template("answer2.html",c=c,ind=wrongimage())
         else:
-            c=True
+            c="True"
             return render_template('page2.html',c=c,ind=wrongimage())
     return render_template("page2.html",c=c,ind=wrongimage())
 
@@ -36,7 +35,7 @@ def round3():
 
 @app.route("/checkr3r3r3r3", methods=["GET", "POST"])
 def checkr3():
-    c = False
+    c = "False"
 
     pwd = request.form['pwd']
 
@@ -44,7 +43,7 @@ def checkr3():
         if pwd == "janetaylor":
             return render_template('answer3.html',c=c,ind=wrongimage())
         else:
-            c = True
+            c = "True"
             return render_template('page3.html',c=c)
     return render_template("page3.html",c=c,ind=wrongimage())
 
@@ -55,13 +54,13 @@ def round4():
 
 @app.route("/checkr4r4t4beruiherig", methods=["GET", "POST"])
 def checkr4():
-    c = False
+    c = "False"
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "thalaforareason":
             return render_template('answer4.html',c=c,ind=wrongimage())
         else:
-            c = True
+            c = "True"
             return render_template('page4.html',c=c,ind=wrongimage())
     return render_template("page4.html",c=c,ind=wrongimage())
 
@@ -71,13 +70,13 @@ def round5():
 
 @app.route("/checkr5jerbgjsbjkdbjkdbjb", methods=["GET", "POST"])
 def checkr5():
-    c = False
+    c = "False"
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "fiftythree":
             return render_template('answer5.html',c=c,ind=wrongimage())
         else:
-            c = True
+            c = "True"
             return render_template('page5.html',c=c,ind=wrongimage())
     return render_template("page5.html",c=c,ind=wrongimage())
 
@@ -87,11 +86,11 @@ def round1():
 
 @app.route("/checkr1hhgsbgjkbkjzsgkn", methods=["GET", "POST"])
 def checkr1():
-    c = False
+    c = "False"
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "aurjaoobt":
-            c = True
+            c = "True"
             return render_template('answer.html',c=c,ind=wrongimage())
         else:
             return render_template('page.html',c=c,ind=wrongimage())
