d8282abc9b9268fe3b38547e7fba6fef1720e4f4
Vishesh <112301037@smail.iitpkd.ac.in>
2024-08-24 23:59:43 +0530

    question 1 to 6 done

--- api/app.py
+++ api/app.py
@@ -4,17 +4,14 @@ import os
 
 
 app = Flask(__name__)
-app.secret_key = "e7193bbd8a4ab1dc8bf29c8de5024e4ab10269d9c3cc59bf475f1486e811c6fd5996a0a0873ddb63b07b"
 
-users = ["weasley@1223414432", "dumbledore@1761837661", "lovegood@1846657233", "granger@280282620", "riddle@-931195728", "malfoy@1081489288"]
-
-@app.route("/")
+@app.route("/svvfhevhjbfjbwknjkbfw")
 def hello_world():
-    return render_template("page.html")
+    return render_template("page2.html")
 
 @app.route("/f54ba756f6fd5a33ff943ed53c979235518b")
 def welcome_to_r2():
-    return render_template("answer.html")
+    return render_template("answer2.html")
 
 
 @app.route("/check", methods=["GET", "POST"])
@@ -22,20 +19,103 @@ def check():
     c2 = False
     ans2 = ""
 
-    user_id = request.form['user_id']
     pwd = request.form['pwd']
 
     if request.method == "POST":
-        if user_id in users:
+        
         # pwd = request.form['pwd']
-            session['user_id'] = user_id
-            if pwd == "https://www.youtube.com/watch?v=jtaqHzUhYdw":
-                c2 = True
-                ans2 = "first link"
-                return welcome_to_r2()
-            else:
-                # return render_template("page.html", c1=c2, ans2=ans2)
-                return render_template('page.html', user_id=session.get('user_id'), c1=c2, ans2=ans2)
+        if pwd == "https://www.youtube.com/watch?v=jtaqHzUhYdw":
+            c2 = True
+            ans2 = "first link"
+            return welcome_to_r2()
         else:
-            return render_template("page.html")
-    return render_template("page.html", c1=c2, ans2=ans2)
+            # return render_template("page.html", c1=c2, ans2=ans2)
+            return render_template('page2.html', user_id=session.get('user_id'), c1=c2, ans2=ans2)
+    return render_template("page2.html", c1=c2, ans2=ans2)
+
+
+@app.route("/szhgjierbgojbdjbvjnvjr3", methods=["GET", "POST"])
+def round3():
+    return render_template('page3.html')
+
+@app.route("/checkr3r3r3r3", methods=["GET", "POST"])
+def checkr3():
+    c3 = False
+    ans3 = ""
+
+    pwd = request.form['pwd']
+
+    if request.method == "POST":
+        
+        # pwd = request.form['pwd']
+        if pwd == "janetaylor":
+            c3 = True
+            ans3 = "first link"
+            return render_template('answer3.html')
+        else:
+            return render_template('page3.html', user_id=session.get('user_id'), c3=c3, ans3=ans3)
+    return render_template("page3.html", c3=c3, ans3=ans3)
+
+
+@app.route("/jzhbcgfbbgxfjghsvbgjk", methods=["GET", "POST"])
+def round4():
+    return render_template('page4.html')
+
+@app.route("/checkr4r4t4beruiherig", methods=["GET", "POST"])
+def checkr4():
+    c4 = False
+    ans4 = ""
+    pwd = request.form['pwd']
+
+    if request.method == "POST":
+        
+        # pwd = request.form['pwd']
+        if pwd == "thalaforareason":
+            c4 = True
+            ans4 = "first link"
+            return render_template('answer4.html')
+        else:
+            return render_template('page4.html', user_id=session.get('user_id'), c4=c4, ans4=ans4)
+    return render_template("page4.html", c4=c4, ans4=ans4)
+
+@app.route("/ikekwgnvaguvagunvrnuvhjgasrjhb", methods=["GET", "POST"])
+def round5():
+    return render_template('page5.html')
+
+@app.route("/checkr5jerbgjsbjkdbjkdbjb", methods=["GET", "POST"])
+def checkr5():
+    c5 = False
+    ans5 = ""
+    pwd = request.form['pwd']
+
+    if request.method == "POST":
+        
+        # pwd = request.form['pwd']
+        if pwd == "fiftythree":
+            c5 = True
+            ans5 = "first link"
+            return render_template('answer5.html')
+        else:
+            return render_template('page5.html', user_id=session.get('user_id'), c5=c5, ans5=ans5)
+    return render_template("page5.html", c5=c5, ans5=ans5)
+
+@app.route("/sdbvvjksbjkdgdzjkfbdrjfvhmn", methods=["GET", "POST"])
+def round1():
+    return render_template('page.html')
+
+@app.route("/checkr1hhgsbgjkbkjzsgkn", methods=["GET", "POST"])
+def checkr1():
+    c1 = False
+    ans1 = ""
+    pwd = request.form['pwd']
+
+    if request.method == "POST":
+        
+        # pwd = request.form['pwd']
+        if pwd == "aurjaoobt":
+            c1 = True
+            ans1 = "first link"
+            return render_template('answer.html')
+        else:
+            return render_template('page.html',  c1=c1, ans1=ans1)
+    return render_template("page1.html", c1=c1, ans1=ans1)
