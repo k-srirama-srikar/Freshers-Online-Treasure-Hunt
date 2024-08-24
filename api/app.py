2d5ff952ed6d327706b788df920ad844dd698388
Vishesh <112301037@smail.iitpkd.ac.in>
2024-08-25 03:49:56 +0530

    skill issue

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from random import randint
+import datetime
 app = Flask(__name__)
 gen=0
 def wrongimage():
@@ -18,14 +19,18 @@ def check():
     global c
     c = 0
     pwd = request.form['pwd']
+    c1=0
+    current_time = datetime.datetime.now()
+    if current_time >= datetime.datetime.combine(current_time.date(), datetime.time(10, 0)):
+        c1=1
     if request.method == "POST":
         
         if pwd == "https://www.youtube.com/watch?v=jtaqHzUhYdw":
-            return render_template("answer2.html",c=c,ind=wrongimage())
+            return render_template("answer2.html",c=c,ind=wrongimage(),c1=c1)
         else:
             c=1
-            return render_template('page2.html',c=c,ind=wrongimage())
-    return render_template("page2.html",c=c,ind=wrongimage())
+            return render_template('page2.html',c=c,ind=wrongimage(),c1=c1)
+    return render_template("page2.html",c=c,ind=wrongimage(),c1=c1)
 
 
 @app.route("/szhgjierbgojbdjbvjnvjr3", methods=["GET", "POST"])
@@ -36,14 +41,18 @@ def round3():
 def checkr3():
     global c
     c = 0
+    ,c1=c1c1=0
+    current_time = datetime.datetime.now()
+    if current_time >= datetime.datetime.combine(current_time.date(), datetime.time(10, 0)):
+        c1=1
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "janetaylor":
-            return render_template('answer3.html',c=c,ind=wrongimage())
+            return render_template('answer3.html',c=c,ind=wrongimage(),c1=c1)
         else:
             c = 1
-            return render_template('page3.html',c=c,ind=wrongimage())
-    return render_template('page3.html',c=c,ind=wrongimage())
+            return render_template('page3.html',c=c,ind=wrongimage(),c1=c1)
+    return render_template('page3.html',c=c,ind=wrongimage(),c1=c1)
 
 
 @app.route("/jzhbcgfbbgxfjghsvbgjk", methods=["GET", "POST"])
@@ -54,14 +63,21 @@ def round4():
 def checkr4():
     global c
     c = 0
+    c1=0
+    current_time = datetime.datetime.now()
+    if current_time >= datetime.datetime.combine(current_time.date(), datetime.time(10, 0)):
+        c1=1
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "thalaforareason":
-            return render_template('answer4.html',c=c,ind=wrongimage())
+            return render_template('answer4.html',c=c,ind=wrongimage(),c1=c1)
+        elif pwd=="cr7" or pwd=="cristianoronaldo":
+            c=1
+            return render_template('page4.html',c=c,ind="cr7.mp4",c1=c1)
         else:
             c = 1
-            return render_template('page4.html', c=c ,ind=wrongimage())
-    return render_template("page4.html",c=c,ind=wrongimage())
+            return render_template('page4.html', c=c ,ind=wrongimage(),c1=c1)
+    return render_template("page4.html",c=c,ind=wrongimage(),c1=c1)
 
 @app.route("/ikekwgnvaguvagunvrnuvhjgasrjhb", methods=["GET", "POST"])
 def round5():
@@ -71,14 +87,18 @@ def round5():
 def checkr5():
     global c
     c = 0
+    c1=0
+    current_time = datetime.datetime.now()
+    if current_time >= datetime.datetime.combine(current_time.date(), datetime.time(10, 0)):
+        c1=1
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "fiftythree":
-            return render_template('answer5.html',c=c,ind=wrongimage())
+            return render_template('answer5.html',c=c,ind=wrongimage(),c1=c1)
         else:
             c = 1
-            return render_template('page5.html',c=c,ind=wrongimage())
-    return render_template("page5.html",c=c,ind=wrongimage())
+            return render_template('page5.html',c=c,ind=wrongimage(),c1=c1)
+    return render_template("page5.html",c=c,ind=wrongimage(),c1=c1)
 
 @app.route("/sdbvvjksbjkdgdzjkfbdrjfvhmn", methods=["GET", "POST"])
 def round1():
@@ -88,14 +108,18 @@ def round1():
 def checkr1():
     global c
     c = 0
+    c1=0
+    current_time = datetime.datetime.now()
+    if current_time >= datetime.datetime.combine(current_time.date(), datetime.time(10, 0)):
+        c1=1
     pwd = request.form['pwd']
     if request.method == "POST":
         if pwd == "aurjaoobt":
-            return render_template('answer.html',c=c,ind=wrongimage())
+            return render_template('answer.html',c=c,ind=wrongimage(),c1=c1)
         else:
             c = 1
-            return render_template('page.html',c=c,ind=wrongimage())
-    return render_template("page.html",c=c,ind=wrongimage())
+            return render_template('page.html',c=c,ind=wrongimage(),c1=c1)
+    return render_template("page.html",c=c,ind=wrongimage(),c1=c1)
 
 @app.route('/')
 def home():
