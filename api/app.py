27bd6b77d7dcc192bdeb9da396b0a37a2ce433f1
K Srirama Srikar <143777264+k-srirama-srikar@users.noreply.github.com>
2024-08-24 20:34:29 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,43 +1,10 @@
-# from flask import Flask, render_template, request, redirect, url_for
-
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def hello_world():
-#     return render_template("page.html")
-
-
-# @app.route("/check", methods=["GET", "POST"])
-# def check():
-#     c2 = False
-#     ans2 = ""
-
-#     if request.method == "POST":
-#         pwd = request.form['pwd']
-#         if pwd == "aurjaoobt":
-#             c1 = True
-#             ans1 = "first link"
-#         else:
-#             return render_template("page.html")
-#     return render_template("page.html", c1=c2, ans2=ans2)
-
-
-
-# if __name__ == '__main__':
-#     app.run()
-
-
-
-
-
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
 import json
 import os
 
 
 app = Flask(__name__)
-
+app.secret_key = "e7193bbd8a4ab1dc8bf29c8de5024e4ab10269d9c3cc59bf475f1486e811c6fd5996a0a0873ddb63b07b"
 
 # DATA_FILE = 'user_data.json'
 
@@ -89,12 +56,14 @@ def check():
     if request.method == "POST":
         if user_id in users:
         # pwd = request.form['pwd']
+            session['user_id'] = user_id
             if pwd == "https://www.youtube.com/watch?v=jtaqHzUhYdw":
                 c2 = True
                 ans2 = "first link"
                 return welcome_to_r2()
             else:
-                return render_template("page.html", c1=c2, ans2=ans2)
+                # return render_template("page.html", c1=c2, ans2=ans2)
+                return render_template('page.html', user_id=session.get('user_id'), c1=c2, ans2=ans2)
         else:
             return render_template("page.html")
     return render_template("page.html", c1=c2, ans2=ans2)
